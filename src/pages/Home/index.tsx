50ed88f2eb2a7c2b75dee2e448385d6f24ff0a44
koringz <ok234@foxmail.com>
2022-04-23 23:38:28 +0800

    菜单权限

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,13 +16,13 @@ for (let i = 0; i < 20; i += 1) {
         key: i,
         name: ['Light', 'Bamboo', 'Little'][i % 3],
         timer: ['Light', 'Bamboo', 'Little'][i % 3],
-        description: 'Everything that has a beginning, has an end.',
+        description: 'Everything that has a beginning, has an end.'
     })
 }
 
 class Home extends React.Component {
     constructor(props: any) {
-        super(props);
+        super(props)
         this.state = {
             // 搜索
             searchFormParams: {},
@@ -43,15 +43,18 @@ class Home extends React.Component {
                 {
                     title: '操作',
                     dataIndex: 'operation',
-                    // render: (text: any, record: any) => {
-                    //     return (
-                    //         <Space size="middle" className='rc-antd-layout-table-operation'>
-                    //             <a key={Math.random().toString(36).substring(1, 14)} onClick={() => this.handleCheck(true, record)}>
-                    //                 <span>查看3</span>
-                    //             </a>
-                    //         </Space>
-                    //     )
-                    // }
+                    render: (text: any, record: any) => {
+                        return (
+                            <Space size="middle" className="rc-antd-layout-table-operation">
+                                <a
+                                    key={Math.random().toString(36).substring(1, 14)}
+                                    onClick={() => this.handleCheck(true, record)}
+                                >
+                                    <span>查看</span>
+                                </a>
+                            </Space>
+                        )
+                    }
                 }
             ],
             dataSource: getDataSource,
@@ -61,7 +64,7 @@ class Home extends React.Component {
             loading: false,
             // 其他
             isCheck: false,
-            snapshot: {},
+            snapshot: {}
         }
         this.handleCheck.bind(this)
     }
@@ -71,15 +74,15 @@ class Home extends React.Component {
     async handleSearch() {
         this.setState({ loading: true })
         const params = this.getParamers()
-        React.$api.getshow && React.$api.getshow(params).then((res: any) => {
-            if (res.data.code == 200) {
-                const DATA = res.data.data
-                this.resetStateData(DATA)
-            }
-            else {
-                message.error(res.data.message)
-            }
-        })
+        React.$api.getshow &&
+            React.$api.getshow(params).then((res: any) => {
+                if (res.data.code == 200) {
+                    const DATA = res.data.data
+                    this.resetStateData(DATA)
+                } else {
+                    message.error(res.data.message)
+                }
+            })
     }
 
     getParamers() {
@@ -87,7 +90,7 @@ class Home extends React.Component {
         const params = { ...this.state, ...searchFormParams }
         return {
             pageSize: params.pageSize,
-            pageNum: params.pageNum,
+            pageNum: params.pageNum
         }
     }
     resetStateData(DATA: any) {
@@ -98,23 +101,25 @@ class Home extends React.Component {
             pageSize: DATA.pageSize
         })
     }
-    changeSearchForm() {
-    }
+    changeSearchForm() {}
 
     /**
      * 页码
-     * @param pageNum 
-     * @param pageSize 
+     * @param pageNum
+     * @param pageSize
      */
     handleOnChange(pageNum: number, pageSize: number) {
-        this.setState((preState, props) => ({ pageSize, pageNum }), () => this.handleSearch())
+        this.setState(
+            (preState, props) => ({ pageSize, pageNum }),
+            () => this.handleSearch()
+        )
     }
     handleShowSizeChange(current: number, pageSize: number) {
         this.setState({ pageSize: pageSize, current: current })
     }
     /**
      * 操作
-     * @returns 
+     * @returns
      */
     handleCheck(bool: boolean, data: any) {
         this.setState({
@@ -135,12 +140,16 @@ class Home extends React.Component {
                 <Content>
                     <SearchForm changeSearchForm={this.changeSearchForm.bind(this)} {...this.props}></SearchForm>
                     <Table
-                        className='rc-antd-layout-pagination rc-antd-layout-table'
+                        className="rc-antd-layout-pagination rc-antd-layout-table"
                         columns={columns}
-                        dataSource={!dataSource ? [] : dataSource.map((item: any, index: number) => {
-                            item.key = item.key || index
-                            return item
-                        })}
+                        dataSource={
+                            !dataSource
+                                ? []
+                                : dataSource.map((item: any, index: number) => {
+                                      item.key = item.key || index
+                                      return item
+                                  })
+                        }
                         pagination={{
                             simple: false,
                             showSizeChanger: true,
@@ -148,14 +157,20 @@ class Home extends React.Component {
                             pageSize: pageSize,
                             total: total,
                             current: pageNum,
-                            showTotal: (count) => {
+                            showTotal: count => {
                                 let pageNum = Math.ceil(count)
                                 return '共' + pageNum + '条'
                             },
                             onChange: this.handleOnChange.bind(this)
                         }}
                     />
-                    {isCheck && <RCDialog isVisible={isCheck} onCancel={() => this.onCancel()} onSure={() => this.onSure()} ></RCDialog>}
+                    {isCheck && (
+                        <RCDialog
+                            isVisible={isCheck}
+                            onCancel={() => this.onCancel()}
+                            onSure={() => this.onSure()}
+                        ></RCDialog>
+                    )}
                 </Content>
             </Layout>
         )
